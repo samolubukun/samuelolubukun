c12474f66c27834e3ab88c58745852a2c6e0e4bb
Olubukun Samuel <samuelolubukun@gmail.com>
2025-04-21 10:37:22 +0100

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -14,7 +14,7 @@ const Experience = () => {
         <span>Experience</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--black)':''}}>25+</div>
+        <div className="circle" style={{color: darkMode?'var(--black)':''}}>30+</div>
         <span  style={{color: darkMode?'white':''}}>Completed </span>
         <span>Projects (Portfolio Projects)</span>
       </div>
